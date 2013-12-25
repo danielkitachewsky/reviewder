a52f28ce0945d6ecef8f6e0e1902908607e5e4fe
Daniel Kitachewsky <daniel.kitachewsky@gmail.com>
2013-12-25 06:49:06 +0100

    fix: use Unicode

--- format_test.py
+++ format_test.py
@@ -8,70 +8,70 @@ import unittest
 
 class FormatTestCase(unittest.TestCase):
   def test_rendering_text(self):
-    text = "toto{%toto%}tata{%toto%}:{%titi%}"
-    self.assertEqual("totoatataa:b",
+    text = u"toto{%toto%}tata{%toto%}:{%titi%}"
+    self.assertEqual(u"totoatataa:b",
                      format.render_text(text,
-                                        toto="a", titi="b", tutu="c"))
+                                        toto=u"a", titi=u"b", tutu=u"c"))
     self.assertRaises(format.RenderingError,
                       format.render_text,
                       text,
-                      toto="a")
+                      toto=u"a")
   
   def test_tokens(self):
     pairs = [
-        ("", set()),
-        ("{%%}", set()),
-        ("{%1%}", set()),
-        ("{toto%}", set()),
-        ("{%toto}", set()),
-        ("{%toto.%}", set()),
-        ("{%to-to%}", set()),
-        ("{%toto%}", {"toto"}),
-        ("{%toto.abc%}", {"toto"}),
-        ("{%_1%}", {"_1"}),
-        ("{%toto%}{%tata%}", {"toto", "tata"}),
-        ("{%toto%}{%toto%}", {"toto"}),
+        (u"", set()),
+        (u"{%%}", set()),
+        (u"{%1%}", set()),
+        (u"{toto%}", set()),
+        (u"{%toto}", set()),
+        (u"{%toto.%}", set()),
+        (u"{%to-to%}", set()),
+        (u"{%toto%}", {u"toto"}),
+        (u"{%toto.abc%}", {u"toto"}),
+        (u"{%_1%}", {u"_1"}),
+        (u"{%toto%}{%tata%}", {u"toto", u"tata"}),
+        (u"{%toto%}{%toto%}", {u"toto"}),
         ]
     for text, tokens in pairs:
       self.assertEqual(set(format.collect_tokens(text)), tokens)
 
   def test_token(self):
-    self.assertEqual("a", format.Token("a", "").label)
-    self.assertEqual("a", format.Token("a", None).label)
-    self.assertEqual("a.abc", format.Token("a", "abc").label)
+    self.assertEqual(u"a", format.Token(u"a", u"").label)
+    self.assertEqual(u"a", format.Token(u"a", None).label)
+    self.assertEqual(u"a.abc", format.Token(u"a", u"abc").label)
 
   def test_member(self):
     class InClass(object):
-      abc = "foo"
+      abc = u"foo"
     class InInit(object):
       def __init__(self):
-        self.abc = "foo"
-    dict_like = {"abc": "foo"}
-    text = "{%a.abc%}"
+        self.abc = u"foo"
+    dict_like = {u"abc": u"foo"}
+    text = u"{%a.abc%}"
     for var in [InClass(), InInit(), dict_like]:
-      self.assertEqual("foo", format._get_member(var, "abc"))
-      self.assertEqual("foo", format.render_text(text, a=var))
-    self.assertEqual("", format.render_text(text, a=1))
+      self.assertEqual(u"foo", format._get_member(var, u"abc"))
+      self.assertEqual(u"foo", format.render_text(text, a=var))
+    self.assertEqual(u"", format.render_text(text, a=1))
 
 
 class FormatFileTestCase(unittest.TestCase):
   def setUp(self):
-    text = "toto{%toto%}tata{%toto%}:{%titi%}"
+    text = u"toto{%toto%}tata{%toto%}:{%titi%}"
     self.filename = _make_random_filename()
     with open(self.filename, "wb") as f:
-      f.write(text)
+      f.write(text.encode("utf-8"))
 
   def tearDown(self):
     os.remove(self.filename)
 
   def test_rendering_file(self):
-    self.assertEqual("totoatataa:b",
+    self.assertEqual(u"totoatataa:b",
                      format.render_template(self.filename,
-                                            toto="a", titi="b", tutu="c"))
+                                            toto=u"a", titi=u"b", tutu=u"c"))
     self.assertRaises(format.RenderingError,
                       format.render_template,
                       self.filename,
-                      toto="a")
+                      toto=u"a")
 
 def _make_random_filename():
   """Returns a filename with a random string.

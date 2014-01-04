37090074d14aca5c3b851a1ca6a7a4d6508f3870
Daniel Kitachewsky <daniel.kitachewsky@gmail.com>
2014-01-04 15:25:38 +0100

    fix: use absolute pathname for templates in order to locate them from win

--- main.py
+++ main.py
@@ -3,46 +3,9 @@
 
 import os
 
-from reviewder import format
 from reviewder import session
+from reviewder import review_format
 from reviewder import review_io
-
-
-def _target_level(review):
-  """Returns a formatted level string for the target."""
-  if review.new_level:
-    return u"%s->%s" % (review.existing_level, review.new_level)
-  else:
-    return review.existing_level
-
-
-def _is_recommendation(review):
-  """Guess if the review is a recommendation towards L3."""
-  text = review.comments.lower() + review.strengths.lower()
-  rec_pos = text.find("recommendation")
-  while rec_pos >= 0:
-    text_around = text[max(0, rec_pos - 15): min(rec_pos + 30, len(text) - 1)]
-    if "level 3" in text_around \
-          or "l3" in text_around \
-          or "level three" in text_around:
-      return True
-    rec_pos = text.find("recommendation", rec_pos + 1)
-  return False
-
-
-def _bgcolor(review):
-  if review.new_level:  # Certification review
-    return "#ffff88"  # Light yellow
-  if _is_recommendation(review):
-    return "#aaffaa"  # Light green
-  return "#ffffff"  # White
-
-
-def _exam_score(review):
-  """Returns an HTML-formatted exam score, if applicable."""
-  if review.type_ == "Evaluation":
-    return u""
-  return u"<p>Scored %s on written exam." % review.exam_score
 
 
 def save_review(review):
@@ -64,17 +27,8 @@ def main():
   for review in reviews:
     save_review(review)
 
-  rendered_reviews = [
-    format.render_template("templates/review.html",
-                           review=review,
-                           target_level=_target_level(review),
-                           bgcolor=_bgcolor(review),
-                           exam_score=_exam_score(review))
-    for review in reviews]
-  full_html = format.render_template(
-    "templates/reviews.html", body="".join(rendered_reviews), title="Reviews",
-    all_review_ids="[%s]" % ",".join(str(r.id_) for r in reviews))
-  print full_html.encode("utf-8")
+  print review_format.render_reviews(reviews,
+                                     title=u"Reviews")
 
 
 if __name__ == "__main__":
---
+++ reviewder/review_format.py
@@ -0,0 +1,67 @@
+
+
+import inspect
+import os
+
+from reviewder import format
+
+
+def _get_template(name):
+  """Finds the template with given name relative to this module."""
+  here = os.path.abspath(inspect.getfile(inspect.currentframe()))
+  return os.path.join(os.path.dirname(here), "templates", name)
+
+
+def _target_level(review):
+  """Returns a formatted level string for the target."""
+  if review.new_level:
+    return u"%s->%s" % (review.existing_level, review.new_level)
+  else:
+    return review.existing_level
+
+
+def _is_recommendation(review):
+  """Guess if the review is a recommendation towards L3."""
+  text = review.comments.lower() + review.strengths.lower()
+  rec_pos = text.find("recommendation")
+  while rec_pos >= 0:
+    text_around = text[max(0, rec_pos - 15): min(rec_pos + 30, len(text) - 1)]
+    if "level 3" in text_around \
+          or "l3" in text_around \
+          or "level three" in text_around:
+      return True
+    rec_pos = text.find("recommendation", rec_pos + 1)
+  return False
+
+
+def _bgcolor(review):
+  if review.new_level:  # Certification review
+    return "#ffff88"  # Light yellow
+  if _is_recommendation(review):
+    return "#aaffaa"  # Light green
+  return "#ffffff"  # White
+
+
+def _exam_score(review):
+  """Returns an HTML-formatted exam score, if applicable."""
+  if review.type_ == "Evaluation":
+    return u""
+  return u"<p>Scored %s on written exam." % review.exam_score
+
+
+def render_review(review):
+  return format.render_template(_get_template("review.html"),
+                                review=review,
+                                target_level=_target_level(review),
+                                bgcolor=_bgcolor(review),
+                                exam_score=_exam_score(review))
+
+
+def render_reviews(reviews, title):
+  rendered_reviews = [render_review(review) for review in reviews]
+  full_html = format.render_template(
+    _get_template("reviews.html"),
+    body="".join(rendered_reviews),
+    title=title,
+    all_review_ids="[%s]" % ",".join(str(r.id_) for r in reviews))
+  return full_html.encode("utf-8")

7c970923363196a2093e8554e408690bf1cbec27
Daniel Kitachewsky <daniel.kitachewsky@gmail.com>
2014-01-04 22:54:39 +0100

    add: ability to download recos

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ def get_review_bundle(dci_number):
   return reviews
 
 
-def main():
-  print "Reviewder, the review downloader!"
+def prompt_review_bundle():
   dci_number = raw_input(
     "Type a DCI number to get all reviews on and by that person\nDCI: ")
   if not re.match("^[1-9][0-9]{3,9}$", dci_number):
@@ -27,11 +26,34 @@ def main():
     sys.exit(1)
   reviews = get_review_bundle(dci_number)
   name = "%s_reviews.html" % dci_number
+  save_to_file(reviews, name)
+
+
+def get_recos():
+  """Get recommendation reviews."""
+  reviews = (session.JudgeCenterSession()
+             .add_filter("SubjectLevelCd", "2")
+             .add_filter_or("Comments", "recommendation")
+             .add_filter("Strengths", "recommendation")
+             .get_reviews(100))
+  return reviews
+
+
+def save_to_file(reviews, name):
   filename = os.path.join(os.path.expanduser("~/Documents"), name)
   with open(filename, "wb") as f:
     f.write(review_format.render_reviews(reviews,
                                          title=u"Reviews"))
   print "Reviews saved in %s" % filename
+
+
+def main():
+  print "Reviewder, the review downloader!"
+  if len(sys.argv) > 1 and sys.argv[1] == "reco":
+    reviews = get_recos()
+    save_to_file(reviews, "recos.html")
+  else:
+    prompt_review_bundle()
   if sys.platform == 'win32':
     raw_input("Press enter to close...")
 

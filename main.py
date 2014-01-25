8415d73872bd5c59e14a77dd1740e1062a01431e
Daniel Kitachewsky <daniel.kitachewsky@gmail.com>
2014-01-25 17:43:23 +0100

    add: command-line argument to limit the number of reviews output

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from reviewder import session
 from reviewder import review_format
 
 
-def get_review_bundle(dci_number):
+def get_review_bundle(dci_number, limit=0):
   """Get all reviews by and on single person."""
   reviews = (session.JudgeCenterSession()
              .add_filter_or("SubjectDCINumber", str(dci_number))
              .add_filter("ReviewerDCINumber", str(dci_number))
-             .get_reviews())
+             .get_reviews(limit))
   return reviews
 
 
-def prompt_review_bundle():
+def prompt_review_bundle(limit=0):
   dci_number = raw_input(
     "Type a DCI number to get all reviews on and by that person\nDCI: ")
   if not re.match("^[1-9][0-9]{3,9}$", dci_number):
     print "This doesn't look like a DCI number. Aborting."
     sys.exit(1)
-  reviews = get_review_bundle(dci_number)
+  reviews = get_review_bundle(dci_number, limit)
   name = "%s_reviews.html" % dci_number
   save_to_file(reviews, name)
 
@@ -56,6 +56,8 @@ def main():
   if len(sys.argv) > 1 and sys.argv[1] == "reco":
     reviews = get_recos()
     save_to_file(reviews, "recos.html")
+  elif len(sys.argv) > 1:
+    prompt_review_bundle(int(sys.argv[1]))
   else:
     prompt_review_bundle()
   if sys.platform == 'win32':
